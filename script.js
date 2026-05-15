eaac04919318323cd4bfea06bf1a8672543f5387
Lukezn <lipegabriel222@gmail.com>
2026-05-15 19:39:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Liquid Glass carregado");
+const enterBtn = document.getElementById("enterBtn");
+
+const login = document.getElementById("login");
+
+const iosUI = document.getElementById("iosUI");
+
+enterBtn.addEventListener("click", () => {
+
+    login.style.opacity = "0";
+
+    login.style.pointerEvents = "none";
+
+    iosUI.classList.add("show");
+
+});
